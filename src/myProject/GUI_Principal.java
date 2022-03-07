787957447eb79bd1bf7322811e6197d5a6bd6670
202040506 Mayra Alejandra Sanchez <mayra.alejandra.sanchez@correounivalle.edu.co>
2022-03-07 11:39:42 -0500

    configuracion de botones y creacion de nueva ventana

--- src/myProject/GUI_Principal.java
+++ src/myProject/GUI_Principal.java
@@ -13,13 +13,25 @@ import java.awt.event.ActionListener;
 public class GUI_Principal extends JFrame {
 
     public static final String PATH ="/recursos/";
+    public static final String AYUDA ="Batalla Naval es un juego tradicional de estrategia y suerte, que involucra a dos participantes (para este caso un jugador vs el computador).\n"+
+            "\nEl objetivo del juego es ser el primero en hundir los barcos del oponente. \n"+"\nCada jugador tiene 2 tableros compuesto por 10 filas y 10 columnas: \n"+
+            "\n-> Tablero de posición: Representa tu territorio, en él distribuirás tu flota antes de comenzar la partida y sólo será de observación. Verás la posición de tus barcos \ny los disparos de tu oponente en tu territorio, pero no podrás realizar ningún cambio ni disparo en él. \n"+
+            "\n-> Tablero principal: Representa el territorio del enemigo, donde tiene desplegada su flota. Será aquí donde se desarrollen los movimientos (disparos) del jugador tratando \nde hundir los barcos enemigos. Este tablero aparecerá en la pantalla del jugador una vez comience la partida y en él quedarán registrados todos sus movimientos, reflejando \ntanto los disparos al agua como los barcos tocados y hundidos hasta el momento. \n"
+            +"\n\nCada jugador tiene una flota de 9 barcos de diferente tamaño, por lo que cada uno ocupará un número determinado de casillas en el tablero: \n"+
+            "\n• 1 portaaviones: ocupa 4 casillas "+"\n• 2 submarinos: ocupan 3 casillas cada uno."+"\n• 3 destructores: ocupan 2 casillas cada uno "+"\n• 4 fragatas: ocupan 1 casilla cada uno "
+            +"\n\nCada barco puede ser ubicado de manera horizontal o vertical en el tablero de posición. "+"\n\nTerminología y movimientos: \n\n"+"• Agua: cuando se dispara sobre una casilla donde no está colocado ningún barco enemigo. En el tablero principal del jugador aparecerá una X. Pasa el turno a tu oponente."+
+            "\n• Tocado: cuando se dispara en una casilla en la que está ubicado un barco enemigo que ocupa 2 o más casillas y se destruye sólo una parte del barco. En el tablero del jugador \naparecerá esa parte del barco con una marca indicativa de que ha sido tocado. El jugador vuelve a disparar. "
+            +"\n• Hundido: si se dispara en una casilla en la que está ubicado una fragata (1 casilla) u otro barco con el resto de casillas tocadas, se habrá hundido, es decir, se ha eliminado ese \nbarco del juego. Aparecerá en el tablero principal del jugador, el barco completo con la marca indicativa de que ha sido hundido. El jugador puede volver a disparar, siempre y cuando no hayas\n hundido toda la flota de su enemigo, en cuyo caso habrá ganado. ";
+    public  static final String CREDITOS="CRÉDITOS\n" +
+            "-> BRAYAN STIVEN SANCHEZ LEON\n" +
+            "-> MAYRA ALEJANDRA SANCHEZ SALINAS";
     private Header headerProject;
     private JButton ayuda, creditos, comenzarPartida, movimientosEnemigo;
     private Escucha escucha;
+    private ImageIcon team, help, enemy, play;
     private JPanel contentPane;
-    private TitledBorder titulo1, titulo2;
-    private Border blackline;
     private PintarTablero pintarTablero;
+    private GUI_Secundaria ventanaEnemy;
 
     /**
      * Constructor of GUI class
@@ -71,13 +83,19 @@ public class GUI_Principal extends JFrame {
         escucha = new Escucha();
 
         // Set up JComponents
+        // Imagenes
+        team = new ImageIcon(getClass().getResource(PATH + "team.png"));
+        help = new  ImageIcon(getClass().getResource(PATH+"signo.png"));
+        play = new ImageIcon(getClass().getResource(PATH+"play.png"));
+        enemy = new ImageIcon(getClass().getResource(PATH+"enemy.png"));
+
         // JComponents de la parte superior
         // Titulo
         headerProject = new Header("B A T A L L A   N A V A L", Color.CYAN);
         panelSup.add(headerProject,FlowLayout.LEFT);
 
         // Creación botón ayuda
-        ayuda = new JButton("", new  ImageIcon(getClass().getResource(PATH+"signo.png")));
+        ayuda = new JButton("", help);
         ayuda.addActionListener(escucha);
         ayuda.setBackground(Color.CYAN);
         ayuda.setFocusable(false);
@@ -85,7 +103,7 @@ public class GUI_Principal extends JFrame {
         panelSup.add(ayuda,FlowLayout.CENTER);
 
         // Creación botón créditos
-        creditos = new JButton("", new ImageIcon(getClass().getResource(PATH+"team.png")));
+        creditos = new JButton("", team);
         creditos.addActionListener(escucha);
         creditos.setBackground(Color.CYAN);
         creditos.setFocusable(false);
@@ -99,7 +117,7 @@ public class GUI_Principal extends JFrame {
 
         //JComponents de la parte Inferior
         // Creación de botón comenzar partida
-        comenzarPartida = new JButton("Comenzar partida", new ImageIcon(getClass().getResource(PATH+"play.png")));
+        comenzarPartida = new JButton("Comenzar partida", play);
         comenzarPartida.addActionListener(escucha);
         comenzarPartida.setBackground(Color.CYAN);
         comenzarPartida.setFocusable(false);
@@ -107,7 +125,7 @@ public class GUI_Principal extends JFrame {
         panelInferior.add(comenzarPartida,FlowLayout.LEFT);
 
         // Creación de botón de movimientos del oponente
-        movimientosEnemigo = new JButton("Movimientos oponente", new ImageIcon(getClass().getResource(PATH+"enemy.png")));
+        movimientosEnemigo = new JButton("Movimientos oponente", enemy);
         movimientosEnemigo.addActionListener(escucha);
         movimientosEnemigo.setBackground(Color.CYAN);
         movimientosEnemigo.setFocusable(false);
@@ -133,7 +151,24 @@ public class GUI_Principal extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            if(e.getSource()==ayuda){
+                JOptionPane.showMessageDialog(null,AYUDA,"¿Como se juega batalla naval?",JOptionPane.PLAIN_MESSAGE, help);
+            }else {
+                if(e.getSource()==creditos){
+                    JOptionPane.showMessageDialog(null,CREDITOS,"Créditos",JOptionPane.PLAIN_MESSAGE, team);
+                }else {
+                    if(e.getSource()==comenzarPartida){
 
+
+
+                    }else {
+                        if(e.getSource()==movimientosEnemigo){
+                            GUI_Secundaria ventanaEnemy = new GUI_Secundaria();
+                            ventanaEnemy.setVisible(true);
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- src/myProject/GUI_Secundaria.java
+++ src/myProject/GUI_Secundaria.java
@@ -1,4 +1,68 @@
 package myProject;
 
-public class GUI_Secundaria {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI_Secundaria extends JFrame {
+    public static final String PATH = "/recursos/";
+    private Header titulo;
+    private JLabel enemy;
+    private ImageIcon enemyIcon;
+
+    public GUI_Secundaria() {
+        initGUI_Secundaria();
+
+        //Default JFrame configuration
+        this.setTitle("Batalla Naval");
+        Image image = new ImageIcon(getClass().getResource(PATH + "barcoIcono.png")).getImage();
+        this.setIconImage(image);
+        this.setUndecorated(false);
+        this.setSize(500, 500);
+        this.setResizable(true);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+    private void initGUI_Secundaria() {
+        // Set up JFrame Container's Layout
+        getContentPane().setLayout(new BorderLayout(0,0));
+
+        JPanel panelPrincipal = new JPanel();
+        panelPrincipal.setBackground(Color.CYAN);
+        getContentPane().add(panelPrincipal,BorderLayout.CENTER);
+        panelPrincipal.setLayout(new BorderLayout(0,0));
+
+        JPanel panelSup = new JPanel();
+        panelSup.setBackground(Color.CYAN);
+        panelPrincipal.add(panelSup,BorderLayout.NORTH);
+        panelSup.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
+
+        JPanel panelInferior = new JPanel();
+        panelInferior.setBackground(Color.CYAN);
+        panelPrincipal.add(panelInferior,BorderLayout.SOUTH);
+        panelInferior.setLayout(new FlowLayout(FlowLayout.CENTER,200,0));
+
+        JPanel panelCentral = new JPanel();
+        panelCentral.setBackground(Color.cyan);
+        panelPrincipal.add(panelCentral,BorderLayout.CENTER);
+        panelCentral.setLayout(new BorderLayout(0,0));
+
+        // Create Listener Object and Control Object
+
+        // Set up JComponents
+        // Titulo
+        titulo = new Header("MOVIMIENTOS ENEMIGO", Color.CYAN);
+        panelSup.add(titulo,FlowLayout.LEFT);
+
+        // Icono
+        enemyIcon = new ImageIcon(getClass().getResource(PATH+"enemy.png"));
+        JLabel enemy = new JLabel(enemyIcon);
+        panelSup.add(enemy,FlowLayout.CENTER);
+
+
+
+
+    }
 }
