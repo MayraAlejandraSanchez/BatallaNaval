d92ff2c793b61899de7cbf37d8bc835a25f7b406
202040506 Mayra Alejandra Sanchez <mayra.alejandra.sanchez@correounivalle.edu.co>
2022-03-08 12:18:00 -0500

    Actualizacion con lo de la rama funcionamiento

--- src/myProject/PintarTablero.java
+++ src/myProject/PintarTablero.java
@@ -9,19 +9,41 @@ public class PintarTablero extends JPanel {
     private String abecedario[];
 
     public PintarTablero(){
-        this.setLayout(new GridLayout(1,2, 10, 0));
+        inicar();
+    }
+
+    public void inicar(){
+        GridBagLayout gb = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        this.setLayout(gb);
+
+        // Panel tablero posicion
         panelTableroPosicion = new JPanel(new GridLayout(11, 11));
+        panelTableroPosicion.setPreferredSize(new Dimension(400, 400));
+        panelTableroPosicion.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+        panelTableroPosicion.setBackground(Color.BLACK);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.insets = new Insets(0,30,0,30);
+        this.add(panelTableroPosicion, gbc);
+
+        // Panel tablero principal
         panelTableroPrincipal = new JPanel(new GridLayout(11, 11));
+        panelTableroPrincipal.setPreferredSize(new Dimension(400, 400));
+        panelTableroPrincipal.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+        panelTableroPrincipal.setBackground(Color.BLACK);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.insets = new Insets(0,30,0,30);
+        this.add(panelTableroPrincipal, gbc);
+
         tableroPosicion = new Tableros();
         tableroPrincipal = new Tableros();
         abecedario = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
-
-        panelTableroPosicion.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panelTableroPosicion.setBackground(Color.BLACK);
-        //panelTableroPosicion.setPreferredSize(new Dimension(200, 200));
-        panelTableroPrincipal.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panelTableroPrincipal.setBackground(Color.BLACK);
-        //panelTableroPrincipal.setPreferredSize(new Dimension(200, 200));
 
         for (int row = 0; row < 11; row++) {
             for (int col = 0; col < 11; col++) {
@@ -54,9 +76,6 @@ public class PintarTablero extends JPanel {
                 panelTableroPrincipal.add(tableroPrincipal.getMatriz()[row][col]);
             }
         }
-
-        add(panelTableroPosicion);
-        add(panelTableroPrincipal);
     }
 
     // Retorna la matriz posicion
